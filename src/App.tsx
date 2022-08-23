9825417b592432896d2cce4ef616fe5ecbb661f3
Marcelo <mrclgst10@gmail.com>
2022-08-23 16:14:16 -0300

    configure initial router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router';
+
 export function App() {
-  return <h1>MCUAPI - Admin</h1>;
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  );
 }
