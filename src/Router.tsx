0ff87eaa673870cda294c54fb8db458045a8ed94
Marcelo <mrclgst10@gmail.com>
2022-09-02 15:51:04 -0300

    add movies routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
+import { DefaultLayout } from './layouts/DefaultLayout';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
+import { Movie } from './pages/Movie';
+import { Movies } from './pages/Movies';
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<Movie />} />
+      </Route>
       <Route path="/login" element={<Login />} />
     </Routes>
   );
